7b069b5142c747b0d83429cd98f0d65c2167464b
AErmolaev <aermolaev10@gmail.com>
2020-10-28 20:28:39 +0300

    Complete task2.py

--- homeworks/lesson5/task2.py
+++ homeworks/lesson5/task2.py
@@ -2,3 +2,14 @@
 Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк, количества
 слов в каждой строке.
 """
+
+my_file = open('viki.txt', 'r')
+
+content = my_file.readlines()
+print(f'Количество строк в файле - {len(content)}')
+
+my_file = open('viki.txt', 'r')
+content = my_file.read().split()
+print(f'Общее количество слов - {len(content)}')
+
+my_file.close()
